462b414675dce3b07a75a87398d8bedc4432d687
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-09 18:00:07 -0500

    se actualizó el método guardarUsuario en la interfaz UsuarioService para utilizar RequestUsuarioDTO

--- src/main/java/com/proyecto/proyectofinal/service/interfaces/UsuarioService.java
+++ src/main/java/com/proyecto/proyectofinal/service/interfaces/UsuarioService.java
@@ -3,10 +3,12 @@ package com.proyecto.proyectofinal.service.interfaces;
 import java.util.List;
 import java.util.Optional;
 
+
+import com.proyecto.proyectofinal.model.dtos.requestDtos.RequestUsuarioDTO;
 import com.proyecto.proyectofinal.model.entities.UsuarioEntity;
 
 public interface UsuarioService {
-    UsuarioEntity guardarUsuario(UsuarioEntity usuario);
+    UsuarioEntity guardarUsuario(RequestUsuarioDTO usuario);
     Optional<UsuarioEntity> buscarPorCedula(String cedula);
     Optional<UsuarioEntity> buscarPorNombreUsuario(String nombreUsuario);
     List<UsuarioEntity> buscarPorInteres(String interes);
