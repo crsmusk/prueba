b137a5d06860991c89474c9baab32bc53b0ea28e
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-09 17:58:42 -0500

    se implementó la clase UsuarioServiceImpl para gestionar las operaciones relacionadas con UsuarioEntity

---
+++ src/main/java/com/proyecto/proyectofinal/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,182 @@
+package com.proyecto.proyectofinal.service.impl;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.proyecto.proyectofinal.model.dtos.requestDtos.RequestUsuarioDTO;
+import com.proyecto.proyectofinal.model.entities.EmailEntity;
+import com.proyecto.proyectofinal.model.entities.InteresEntity;
+import com.proyecto.proyectofinal.model.entities.RolEntity;
+import com.proyecto.proyectofinal.model.entities.TelefonoEntity;
+import com.proyecto.proyectofinal.model.entities.UsuarioEntity;
+import com.proyecto.proyectofinal.model.idsEmbedded.IdTelefono;
+import com.proyecto.proyectofinal.repository.UsuarioRepository;
+import com.proyecto.proyectofinal.service.interfaces.UsuarioService;
+
+
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Value ("${file.upload-dir}")
+    String direccion;
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private RolServiceImpl rolService;
+
+    @Autowired
+    private TelefonoServiceImpl telefonoService;
+
+    @Autowired
+    private InteresServiceImpl interesService;
+
+    @Transactional
+    @Override
+    public UsuarioEntity guardarUsuario(RequestUsuarioDTO usuario) {
+
+        UsuarioEntity usuarioEntity = new UsuarioEntity();
+        usuarioEntity.setCedula(usuario.getCedula());
+        usuarioEntity.setNombreUsuario(usuario.getNombreUsuario());
+        usuarioEntity.setApellidoUsuario(usuario.getApellidoUsuario());
+        usuarioEntity.setContrasena(usuario.getContrasena());
+        
+
+        List<EmailEntity> emails = new ArrayList<>();
+        if (usuario.getEmails() != null) {
+            for (String email : usuario.getEmails()) {
+                EmailEntity emailEntity = new EmailEntity();
+                emailEntity.setEmail(email);
+                emailEntity.setUsuario(usuarioEntity);
+                emails.add(emailEntity);
+            }
+        }
+        usuarioEntity.setEmails(emails);
+        
+        
+        List<RolEntity> roles = buscarRoles(usuario.getRoles());
+        usuarioEntity.setRoles(roles);
+        
+        //se llama al metodo guardarFoto para guardar la foto de perfil
+        usuarioEntity.setFotoPerfilReferencia(guardarFoto(usuario.getFotoPerfil()));
+
+        //se llama al metodo optenerIntereses para obtener los intereses del usuario
+        usuarioEntity.setIntereses(optenerIntereses(usuario.getIntereses()));
+
+
+        usuarioEntity = usuarioRepository.save(usuarioEntity);
+        
+
+        List<TelefonoEntity> telefonos = new ArrayList<>();
+        if (usuario.getTelefonos() != null) {
+            for (String numeroTelefono : usuario.getTelefonos()) {
+                TelefonoEntity telefonoEntity = new TelefonoEntity();
+                IdTelefono idTelefono = new IdTelefono(numeroTelefono, usuarioEntity.getCedula());
+                telefonoEntity.setId(idTelefono);
+                telefonoEntity.setUsuario(usuarioEntity);
+                telefonos.add(telefonoService.guardarTelefono(telefonoEntity));
+            }
+        }
+        usuarioEntity.setTelefonos(telefonos);
+        
+        
+        return usuarioEntity;
+
+    }
+    
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<UsuarioEntity> buscarPorCedula(String cedula) {
+        
+        return this.usuarioRepository.findById(cedula);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<UsuarioEntity> buscarPorNombreUsuario(String nombreUsuario) {
+        return this.usuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UsuarioEntity> buscarPorInteres(String interes) {
+       
+        return this.usuarioRepository.buscarPorInteres(interes);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<UsuarioEntity> listarTodos() {
+        return this.usuarioRepository.findAll();
+    }
+
+    @Override
+    public void eliminarUsuario(String cedula) {
+        if (this.usuarioRepository.existsById(cedula)) {
+            this.usuarioRepository.deleteById(cedula);
+        } 
+    }
+
+    public List<RolEntity> buscarRoles(List<String> roles) {
+        List<RolEntity> rolesEncontrados = new ArrayList<>();
+        for (String rol : roles) {
+            Optional<RolEntity> rolEncontrado = this.rolService.buscarPorNombre(rol);
+            if (rolEncontrado.isPresent()) {
+                rolesEncontrados.add(rolEncontrado.get());
+            }
+        }
+        
+        return rolesEncontrados;
+        
+    }
+
+    public String guardarFoto(MultipartFile archivo) {
+        if (archivo == null || archivo.isEmpty()) {
+            return null;
+        }
+        try {
+            File directorio = new File(direccion);
+            if (!directorio.exists()) {
+                directorio.mkdirs();
+            }
+
+            String nombreOriginal = archivo.getOriginalFilename();
+            String extension = nombreOriginal.substring(nombreOriginal.lastIndexOf("."));
+            String nombreArchivo = System.currentTimeMillis() + extension;
+
+            Path path = Paths.get(direccion, nombreArchivo);
+            Files.write(path, archivo.getBytes());
+
+            return "/fotos/" + nombreArchivo;
+        } catch (Exception e) {
+            System.out.println("Error al guardar la imagen: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<InteresEntity> optenerIntereses(List<String> intereses) {
+        List<InteresEntity> interesesEncontrados = new ArrayList<>();
+        for (String interes : intereses) {
+            Optional<InteresEntity> interesEncontrado = this.interesService.buscarPorNombre(interes);
+            if (interesEncontrado.isPresent()) {
+                interesesEncontrados.add(interesEncontrado.get());
+            }
+        }
+        
+        return interesesEncontrados;
+    }
+    
+}
