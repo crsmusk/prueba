0355029d3af318ceb4876ae11e5d2dca16ad9928
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-09 17:51:23 -0500

    se creó el DTO RequestUsuarioDTO para manejar mejor la información del usuario en las solicitudes

---
+++ src/main/java/com/proyecto/proyectofinal/model/dtos/requestDtos/RequestUsuarioDTO.java
@@ -0,0 +1,40 @@
+package com.proyecto.proyectofinal.model.dtos.requestDtos;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestUsuarioDTO {
+    
+    @Size(max = 20)
+    private String cedula;
+    
+    @Size(max = 20)
+    private String nombreUsuario;
+    
+    @Size(max = 20)
+    private String apellidoUsuario;
+    
+    @Size(max = 15)
+    private String contrasena;
+    
+    private MultipartFile fotoPerfil;
+    
+    private List<String> telefonos;
+    
+    private List<String> emails;
+    
+    private List<String> intereses;
+    
+    private List<String> roles;
+}
