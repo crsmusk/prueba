22a8d4eeca84a4cafb5e32747a5cf2b066ce8520
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-10 18:19:43 -0500

    se agregó la funcionalidad para inscribir usuarios en actividades y se implementó un método programado para enviar recordatorios de actividades

--- src/main/java/com/proyecto/proyectofinal/service/impl/ActividadServiceImpl.java
+++ src/main/java/com/proyecto/proyectofinal/service/impl/ActividadServiceImpl.java
@@ -5,13 +5,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.proyecto.proyectofinal.model.entities.ActividadEntity;
+import com.proyecto.proyectofinal.model.entities.EmailEntity;
+import com.proyecto.proyectofinal.model.entities.UsuarioEntity;
 import com.proyecto.proyectofinal.model.idsEmbedded.IdActividad;
 import com.proyecto.proyectofinal.repository.ActividadRepository;
 import com.proyecto.proyectofinal.service.interfaces.ActividadService;
+import com.proyecto.proyectofinal.service.interfaces.UsuarioService;
+
+import jakarta.persistence.Id;
 
 
 
@@ -19,13 +25,23 @@ import com.proyecto.proyectofinal.service.interfaces.ActividadService;
 public class ActividadServiceImpl implements ActividadService {
 
     @Autowired
+    private EmailServiceImpl emailService;
+
+    @Autowired
     private ActividadRepository actividadRepository;
+    
+    @Autowired
+    private UsuarioService usuarioService;
     
     @Transactional
     @Override
     public ActividadEntity guardarActividad(ActividadEntity actividad) {
+        IdActividad idActividad = actividad.getId();
+        idActividad.setFechaCreacion(LocalDateTime.now());
         ActividadEntity actividadGuardada = this.actividadRepository.findById(actividad.getId())
             .orElse( actividadRepository.save(actividad));
+
+        actividadGuardada.setId(idActividad);
         return actividadGuardada;
     }
 
@@ -68,5 +84,42 @@ public class ActividadServiceImpl implements ActividadService {
     @Override
     public void eliminarActividad(IdActividad id) {
         actividadRepository.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public ActividadEntity inscribirUsuario(IdActividad idActividad, String cedulaUsuario)  {
+      
+        ActividadEntity actividad = this.actividadRepository.findById(idActividad).orElse(null);
+           
+        UsuarioEntity usuario = this.usuarioService.buscarPorCedula(cedulaUsuario).orElse(null);
+       
+
+        if (actividad.getCapacidad() > 0 && !actividad.getParticipantes().contains(usuario)) {
+
+           actividad.getParticipantes().add(usuario);
+        }  
+        
+        return actividadRepository.save(actividad);
+    }
+
+    @Scheduled(cron = "0 0 12 * * *") //se espera que se ejectute todos los dias alas 12
+    @Transactional(readOnly = true)
+    public void enviarRecordatoriosActividades() {
+        LocalDateTime mañana = LocalDateTime.now().plusDays(1).withHour(0).withMinute(0);
+        LocalDateTime pasadoMañana = mañana.plusDays(1);
+        
+        List<ActividadEntity> actividadesManana = actividadRepository.findActivitiesBetweenDates(mañana, pasadoMañana);
+        
+        for (ActividadEntity actividad : actividadesManana) {
+            for (UsuarioEntity participante : actividad.getParticipantes()) {
+                emailService.enviarRecordatorio(
+                    participante.getEmails().stream().map(EmailEntity::getEmail).toList(),
+                    actividad.getNombreActividad(),
+                    actividad.getId().getFechaInicio().toString(),
+                    actividad.getDireccionActividad().toString()
+                );
+            }
+        }
     }
 }
