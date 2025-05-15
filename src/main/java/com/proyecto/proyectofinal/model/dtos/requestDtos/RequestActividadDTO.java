236c40f8c3acd71402a99714fa9974b696e74671
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-15 17:28:35 -0500

    se agregaron los campos cedulaCreador, ciudad y direccion en el DTO RequestActividadDTO

--- src/main/java/com/proyecto/proyectofinal/model/dtos/requestDtos/RequestActividadDTO.java
+++ src/main/java/com/proyecto/proyectofinal/model/dtos/requestDtos/RequestActividadDTO.java
@@ -6,10 +6,13 @@ import lombok.Data;
 
 @Data
 public class RequestActividadDTO {
+    private String cedulaCreador;
     private String nombre;
     private String descripcion;
     private LocalDateTime fechaInicio;
     private String ubicacion;
     private int capacidad;
     private List<String> intereses;
+    private String ciudad;
+    private String direccion;
 }
