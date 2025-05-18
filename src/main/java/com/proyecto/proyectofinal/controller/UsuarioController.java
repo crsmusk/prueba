00aeee11b206ad040aebd19e9e555e8d6bc8d202
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-18 18:54:24 -0500

    se refactorizó el controlador UsuarioController para mejorar la gestión de usuarios y corregir redirecciones

--- src/main/java/com/proyecto/proyectofinal/controller/UsuarioController.java
+++ src/main/java/com/proyecto/proyectofinal/controller/UsuarioController.java
@@ -1,23 +1,17 @@
 package com.proyecto.proyectofinal.controller;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.proyecto.proyectofinal.model.dtos.requestDtos.RequestUsuarioDTO;
-import com.proyecto.proyectofinal.service.interfaces.EmailService;
 import com.proyecto.proyectofinal.service.interfaces.UsuarioService;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 @RequestMapping("/usuario")
@@ -26,21 +20,43 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-
     @GetMapping("/registro")
     public String mostrarFormularioRegistro(Model model) {
         model.addAttribute("usuarioDto", new RequestUsuarioDTO());
-        return "index";
+        return "crearUsuario";
+    }
+
+    @GetMapping("/perfil-creador/{cedula}")
+    public String mostrarPerfilCreador(@PathVariable String cedula, Model model) {
+        model.addAttribute("usuario", this.usuarioService.buscarPorCedula(cedula));
+
+        return "infoUsuarioCreador";
+    }
+
+    @GetMapping("/perfil-normal/{cedula}")
+    public String mostrarPerfilNormal(@PathVariable String cedula, Model model) {
+        model.addAttribute("usuario", this.usuarioService.buscarPorCedula(cedula));
+
+        return "infoUsuarioCreador";
     }
 
     @PostMapping("/guardar")
     public String guardarUsuario(@ModelAttribute("usuarioDto") RequestUsuarioDTO usuarioDto,
             RedirectAttributes redirectAttributes) {
         this.usuarioService.guardarUsuario(usuarioDto);
-        
-        return "redirect:/usuario/registro";
+
+        return "redirect:/usuario/crearUsuario";
     }
-     
-   
-    
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/borrar-usuario/{cedula}")
+    public String borrarUsuario(@PathVariable String cedula, Model model) {
+        this.usuarioService.eliminarUsuario(cedula);
+        return "redirect:/actividad/listar-actividades";
+    }
+
 }
