8768c6740dc3fb5a2f5effe42aca10cebc6f67b9
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-18 18:57:28 -0500

    se refactorizó la interfaz ActividadService para reemplazar IdActividad por LocalDateTime en los métodos de búsqueda y se actualizaron los tipos de retorno a ResponseActividadDTO

--- src/main/java/com/proyecto/proyectofinal/service/interfaces/ActividadService.java
+++ src/main/java/com/proyecto/proyectofinal/service/interfaces/ActividadService.java
@@ -1,21 +1,25 @@
 package com.proyecto.proyectofinal.service.interfaces;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import com.proyecto.proyectofinal.model.dtos.requestDtos.RequestActividadDTO;
+import com.proyecto.proyectofinal.model.dtos.responseDtos.ResponseActividadDTO;
 import com.proyecto.proyectofinal.model.entities.ActividadEntity;
-import com.proyecto.proyectofinal.model.idsEmbedded.IdActividad;
 
 public interface ActividadService {
     ActividadEntity guardarActividad(RequestActividadDTO actividadDto);
-    Optional<ActividadEntity> buscarPorId(IdActividad id);
-    List<ActividadEntity> buscarPorCiudad(String ciudad);
-    List<ActividadEntity> buscarPorInteres(String interes);
-    List<ActividadEntity> buscarPorCreador(String cedula);
-    List<ActividadEntity> buscarEntreFechas(LocalDateTime inicio);
-    List<ActividadEntity> buscarProximasActividades();
-    void eliminarActividad(IdActividad id);
-    ActividadEntity inscribirUsuario(IdActividad idActividad, String cedulaUsuario);
+    ResponseActividadDTO buscarPorId(LocalDateTime fechaCreacion);
+    List<ResponseActividadDTO> buscarPorCiudad(String ciudad);
+    List<ResponseActividadDTO> buscarPorInteres(String interes);
+    List<ResponseActividadDTO> buscarPorCreador(String cedula);
+    List<ResponseActividadDTO> buscarEntreFechas(LocalDateTime inicio);
+    List<ResponseActividadDTO> buscarProximasActividades();
+    List<ResponseActividadDTO> buscarPorNombreActividad(String nombre);
+    List<ResponseActividadDTO> buscarPorParticipante(String cedula);
+    void eliminarActividad(LocalDateTime fechaCreacion);
+    void actualizarActividad(LocalDateTime fechaCreacion, RequestActividadDTO actividadDto)throws IOException;
+    ActividadEntity inscribirUsuario(LocalDateTime fechaCreacion, String cedulaUsuario);
 }
