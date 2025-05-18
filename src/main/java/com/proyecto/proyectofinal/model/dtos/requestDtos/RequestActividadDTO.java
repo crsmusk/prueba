85408c33fd2b25ac48f874f1daa7e86a05c3e46f
cristian mosquera trullo <cristiantrullo41@gmail.com>
2025-05-18 18:55:01 -0500

    se actualizaron los campos del DTO RequestActividad para incluir fecha de creación y foto de actividad

--- src/main/java/com/proyecto/proyectofinal/model/dtos/requestDtos/RequestActividadDTO.java
+++ src/main/java/com/proyecto/proyectofinal/model/dtos/requestDtos/RequestActividadDTO.java
@@ -2,7 +2,12 @@ package com.proyecto.proyectofinal.model.dtos.requestDtos;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 @Data
 public class RequestActividadDTO {
@@ -10,9 +15,11 @@ public class RequestActividadDTO {
     private String nombre;
     private String descripcion;
     private LocalDateTime fechaInicio;
-    private String ubicacion;
+    private LocalDateTime fechaCreacion;
+    private String fechaCreacionTexto;
     private int capacidad;
     private List<String> intereses;
     private String ciudad;
     private String direccion;
+    private MultipartFile fotoActividad;
 }
